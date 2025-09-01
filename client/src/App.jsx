065eede04b2928692f441f7bffdb15a5f447811d
SYED IMRAN HASSAN <syedimranh59@gmail.com>
2025-09-01 23:03:48 +0530

    added margin top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-blue-500 text-white p-4'>
+    <div className='bg-blue-500 text-white p-4 mt-10'>
       jhjhgjhg
     </div>
   )
