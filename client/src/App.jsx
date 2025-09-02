c88366b4c032018996f88843118a2cff703d3e23
Md Alkama <mdalkamadad@gmail.com>
2025-09-02 21:34:53 +0530

    delete everything

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import LoginSystem from './pages/Login'
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, AuthContext } from "./contexts/AuthProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
-const App = () => {
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  if (loading) return <p>Loading...</p>;
+  return user ? children : <Navigate to="/" />;
+};
+
+function App() {
   return (
-    <div>
-      <LoginSystem />
-    </div>
-  )
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 dotenv.config({quiet: true});
-connectDB();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 // Start Server
 app.listen(PORT, () => {
