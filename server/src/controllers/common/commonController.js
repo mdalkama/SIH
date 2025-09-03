c9e9f78984da684b86cb953b6a924d5614cc5df4
Md Alkama <mdalkamadad@gmail.com>
2025-09-03 19:14:08 +0530

    send msg and user data

--- server/src/controllers/common/commonController.js
+++ server/src/controllers/common/commonController.js
@@ -3,7 +3,10 @@ import { role } from "../../middlewares/authMiddleware.js";
 
 export const getRole = async (req, res) => {
     try {
-        res.status(200).json({ role: req.user.role , id: req.user.id});
+        res.status(200).json({ 
+                    message: "Access denied: You don't have permission",
+                    user: req.user // optional
+                });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
