273df1074d5855d880a057218ffcd43624216502
Md Alkama <mdalkamadad@gmail.com>
2025-09-03 10:05:32 +0530

    change in cookie

--- server/src/controllers/staffController/staffController.js
+++ server/src/controllers/staffController/staffController.js
@@ -55,7 +55,7 @@ export const loginStaff = async (req, res) => {
         // Set HttpOnly cookie
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
+            secure: true,
             sameSite: "none",
             maxAge: 8 * 60 * 60 * 1000 // 8 hours
         });
