39ba06f05ed2a9cf76a10ca02d2faece1e72076d
Md Alkama <mdalkamadad@gmail.com>
2025-09-04 19:18:13 +0530

    fixed staff login

--- server/src/controllers/staffController/staffController.js
+++ server/src/controllers/staffController/staffController.js
@@ -49,7 +49,7 @@ export const loginStaff = async (req, res) => {
         const token = jwt.sign(
             {
                 message: "Login successful",
-                user: { id: student._id, role: student.role , staffId: staff.staffId}
+                user: { id: staff._id, role: staff.role , staffId: staff.staffId}
             },
             process.env.JWT_SECRET,
             { expiresIn: process.env.JWT_EXPIRES_IN || "8h" }
