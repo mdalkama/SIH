537ebc3ce38f0f1f0b0da155f5cf40af54012fe5
Kahkashann <kahkashanjaween786@gmail.com>
2025-09-15 18:57:35 +0530

    add for student - registraions for exam and get results

---
+++ server/src/routes/studentExamRoute.js
@@ -0,0 +1,23 @@
+// routes/examRoute.js
+import express from "express";
+import { getExamResult, getMyRegistrations, getStudentExams, registerForExam } from "../controllers/studentExamController/studentExamController.js";
+
+
+const router = express.Router();
+
+router.get("/my-exams", getStudentExams);
+// Register for an exam
+router.post("/:examId/register", registerForExam); 
+
+// Get a list of exams the student is already registered for
+router.get("/my-registrations", getMyRegistrations);
+
+// Get the result of a specific past exam
+router.get("/result/:examId", getExamResult); 
+
+// Results
+// router.post("/:id/result", addResult);                     // Add student result
+// router.put("/:id/result/:studentId", updateResult);        // Update student result
+// router.get("/:id/result/:studentId", getStudentResult); // Get specific student result
+
+export default router;
